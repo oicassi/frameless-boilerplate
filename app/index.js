96c38120b0b75cd85f3ae40a9c8c9363f12d5276
Cassiano Kruchelski Vidal <casst@protonmail.com>
2022-03-09 01:20:14 -0300

    add plugins

---
+++ app/index.js
@@ -0,0 +1,3 @@
+import image from 'assets/15.png';
+
+console.log(image);
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ const webpack = require('webpack');
 
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 const IS_DEVELOPMENT = process.env.NODE_ENV === 'dev';
 
 const dirApp = path.join(__dirname, 'app');
-const dirAssets = path.join(__dirname, 'assets');
 const dirStyles = path.join(__dirname, 'styles');
 const dirNode = 'node_modules';
 
@@ -15,7 +15,7 @@ module.exports = {
   entry: [path.join(dirApp, 'index.js'), path.join(dirStyles, 'index.scss')],
 
   resolve: {
-    modules: [dirApp, dirAssets, dirNode],
+    modules: [dirApp, dirNode],
   },
 
   plugins: [
@@ -28,8 +28,78 @@ module.exports = {
         {
           from: './shared',
           to: '',
+          noErrorOnMissing: true,
         },
       ],
     }),
+
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '',
+            },
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'postcss-loader',
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|svg|woff2?|fnt|webp)$/,
+        loader: 'file-loader',
+        options: {
+          name(file) {
+            return '[hash].[ext]';
+          },
+        },
+      },
+
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        type: 'asset',
+      },
+    ],
+  },
+
+  optimization: {
+    minimizer: [
+      new ImageMinimizerPlugin({
+        minimizer: {
+          implementation: ImageMinimizerPlugin.imageminMinify,
+          options: {
+            plugins: [
+              ['gifsicle', { interlaced: true }],
+              ['jpegtran', { progressive: true }],
+              ['optipng', { optimizationLevel: 5 }],
+            ],
+          },
+        },
+      }),
+    ],
+  },
 };
