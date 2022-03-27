b581f17213e664ae7bf1dbb226cf0de4b232b980
Cassiano Kruchelski Vidal <casst@protonmail.com>
2022-03-26 22:21:44 -0300

    add server and scripts to package.json

---
+++ app.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`)
+})
