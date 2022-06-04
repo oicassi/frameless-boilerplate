7d2c15f66d023ada91957f277ccf5053d828ccb7
Cassiano Kruchelski <casst@protonmail.com>
2022-06-04 08:06:30 -0300

    adjust prismic config

--- config/prismicConfig.js
+++ config/prismicConfig.js
@@ -3,35 +3,18 @@ import fetch from 'node-fetch'
 import * as prismic from '@prismicio/client'
 dotenv.config()
 
-const init = () => {
-  try {
-    const repoName = process.env.PRISMIC_REPO
-    const accessToken = process.env.PRISMIC_ACCESS_TOKEN
+const repoName = process.env.PRISMIC_REPO
+const accessToken = process.env.PRISMIC_ACCESS_TOKEN
 
-    if (!repoName) throw new Error('No reponame set in .env')
-    if (!accessToken) throw new Error('No access token set in .env')
+const routes = [
+  {
+    type: 'about',
+    path: '/about',
+  },
+]
 
-    const routes = [
-      {
-        type: 'page',
-        path: '/:uid'
-      }
-    ]
-    const client = prismic.createClient(repoName, {
-      fetch,
-      accessToken,
-      routes
-    })
-    return client
-  } catch (error) {
-    console.log('Error configuring prismic')
-    console.log(error)
-    return {}
-  }
-}
-
-
-const client = init()
-
-
-export { client }
+export const prismicClient = prismic.createClient(repoName, {
+  fetch,
+  accessToken,
+  routes,
+})
